5cdf581993ea6b96358a3b74969d3c5473ad3604
Mark Butler <mbutler@styleseat.com>
2023-06-11 14:03:39 -0400

    Created needed certificate configs...

--- infra/PKI/generate_certs.sh
+++ infra/PKI/generate_certs.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+# Generate CA Certs
 cfssl gencert -initca ca-csr.json | cfssljson -bare ca
+
+# Generate Admin User Certs
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -profile=kubernetes \
+  admin-csr.json | cfssljson -bare admin
+
+  # 
